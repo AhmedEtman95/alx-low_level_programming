bfbbab42f0706757b2931aae1a6ea79cf6ccc790
AhmedEtman95 <ahmed_light.1995@yahoo.com>
2023-02-19 17:03:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,15 @@
 
 /**
  * main - prints"Programming is like building a multilingual
- * puzzle, followed by a new line."
- 
- * Return: Always 0 (Success)
+ * puzzle, followed by a new line." 
+ * Return: Always 0.
  */
+
+
 int main(void)
+
 {
-   puts("\"Programming is like building a multilingual puzzle");
+   
+puts("\"Programming is like building a multilingual puzzle");
     return(0);
 }
