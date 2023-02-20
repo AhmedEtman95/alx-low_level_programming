37ea1f577dba89b20d5c437ae89c5e2926a9ae55
AhmedEtman95 <123496833+AhmedEtman95@users.noreply.github.com>
2023-02-21 00:42:35 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,14 @@
 #include <stdlib.h>
 
-
 #include <time.h>
-
 
 #include <stdio.h>
 
-
-
- /**
-  * main - Prints a text according number
-  * Return: Always (Success)
-  *
-  */
+/**
+* main - Prints a text according number
+* Return: Always (Success)
+*
+*/
 
 int main(void)
 
