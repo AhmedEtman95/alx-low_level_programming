a9ac51b5d339de9b2ea74c6cd08de3e176e31f23
AhmedEtman95 <ahmed_light.1995@yahoo.com>
2023-02-21 02:11:33 +0200

    9-print_comb.c fixing error 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,14 @@ int main(void)
 {
 	int c;
 
-	for (c = 0; c <= 8; c++)
+	for (c = 0; c <= 9; c++)
 	{
 		putchar(c + '0');
-		putchar(',');
+		if (c < 9)
+		{
+			putchar(',');
+		}
 		putchar(' ');
 	}
-	putchar(c + '0');
 	return (0);
 }
