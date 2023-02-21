a7fea419f0c9e210fd54fa8f6f6924b233dde9cd
AhmedEtman95 <ahmed_light.1995@yahoo.com>
2023-02-21 03:04:34 +0200

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,27 +10,33 @@ int main(void)
 
 {
 	int c;
-	int c2;
 
-	int c3;
-	int c4;
 	for (c = 0; c <= 9; c++)
 	{
+		int c2;
+
 		for (c2 = 0; c2 <= 8; c2++)
 		{
+			int c3;
+
 			for (c3 = c; c3 <= 9; c3++)
 			{
-				for (c4 = c2+1; c4 <= 9; c4++)
+				int c4;
+
+				for (c4 = 0; c4 <= 9; c4++)
 				{
-					putchar(c + '0');
-					putchar(c2 + '0');
-					putchar(' ');
-					putchar(c3 + '0');
-					putchar(c4 + '0');
-					if (!(c == 9 && c2 == 8 && c3 == 9 && c4 == 9))
+					if (c3> c ||(c3 == c && c4>c2))
 					{
-						putchar(',');
+						putchar(c + '0');
+						putchar(c2 + '0');
 						putchar(' ');
+						putchar(c3 + '0');
+						putchar(c4 + '0');
+						if (!(c == 9 && c2 == 8 && c3 == 9 && c4 == 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
 					}
 				}
 			}
