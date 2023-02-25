a970d672225121f8864432af10f30b194a84d10c
AhmedEtman95 <ahmed_light.1995@yahoo.com>
2023-02-26 01:55:12 +0200

    error fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <main.h>
 
 /**
 * print_alphabet_x10 - Prints the alphabet in lowercase 10 times, followed by a new line
@@ -10,7 +11,5 @@ void print_alphabet_x10(void)
 	char c;
 	int i;
 	for (i = 0; i < 10; i++)
-		for (c = 'a'; c <= 'z'; c++)
-			putchar(c);
-	putchar('\n');
+		print_alphabet();
 }
