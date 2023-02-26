b6bec649f94676d1b8f11d4a962d557de6c1b8ff
AhmedEtman95 <ahmed_light.1995@yahoo.com>
2023-02-26 02:38:13 +0200

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* _islower - Checks if i is a lower case or not.
+*
+*/
+
+int _islower(int i)
+{
+	int result;
+
+	result = is
+	if (i >= 97 && i <= 122)
+		result = 1;
+	else
+		result = 0;
+
+	return result;
+}
