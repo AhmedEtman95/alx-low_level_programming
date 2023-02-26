8c1712d2148b09390f830b15aa1f91c6d179ecbc
AhmedEtman95 <ahmed_light.1995@yahoo.com>
2023-02-26 02:25:26 +0200

    fix style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
-* print_alphabet_x10 - Prints the alphabet in lowercase 10 times, followed by a new line
+* print_alphabet_x10 - Prints the alphabet in lowercase 10 times,
+* followed by a new line
 *
 */
 
 void print_alphabet_x10(void)
 {
+
 	char c;
 	int i;
 	for (i = 0; i < 10; i++)
