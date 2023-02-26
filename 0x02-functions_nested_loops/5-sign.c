e18d0fc0d11e88c4bbcbf47c58567804de0164c5
AhmedEtman95 <ahmed_light.1995@yahoo.com>
2023-02-26 03:17:39 +0200

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* print_sign - prints sign
+*
+* @i: the integer number that you want to check as per ASCII
+*
+* Return: 1 if greater than 0, 0 if 0 and -1 if less than 0
+*/
+
+int print_sign(int i)
+{
+	int result;
+
+	if (i > 0)
+	{
+		result = 1;
+		putchar('+');
+	}
+	else if (i == 0)
+	{
+		result = 0;
+		putchar('0');
+	}
+	else
+	{
+		result = -1;
+		putchar('-');
+
+	}
+
+	return (result);
+}
